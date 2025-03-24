6430309206acfa7ab564e6a287c51855cf061e8c
tfs47 <trishafsaini@gmail.com>
2025-03-24 14:31:21 -0400

    added historical eras based upon year

--- backend/app.py
+++ backend/app.py
@@ -9,13 +9,37 @@ import pandas as pd
 from processor import WeightedTfidfProcessor
 from filters import Filters
 
-os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
+os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..", os.curdir))
+
+
+def assign_era(year_str):
+
+    year_num = int(year_str)
+
+    if year_num <= -3000:
+        return "Prehistoric"
+    elif year_num <= -1000:
+        return "Bronze Age"
+    elif year_num <= 0:
+        return "Iron Age / Classical"
+    elif year_num <= 500:
+        return "Classical Antiquity"
+    elif year_num <= 1500:
+        return "Medieval"
+    elif year_num <= 1800:
+        return "Early Modern"
+    elif year_num <= 1945:
+        return "Colonial / Industrial"
+    else:
+        return "Contemporary"
+
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 json_file_path = os.path.join(current_directory, 'init.json')
 csv_file_path = os.path.join(current_directory, 'data', 'final_data.csv')
 
 historical_df = pd.read_csv(csv_file_path)
+historical_df['era'] = historical_df['Year'].apply(assign_era)
 
 
 weight_processor = WeightedTfidfProcessor(
@@ -26,10 +50,10 @@ weight_processor = WeightedTfidfProcessor(
 
 def tfidf_search(query, top_n=5):
     return weight_processor.search(query, top_n=top_n)
-    
+
+
 app = Flask(__name__)
 CORS(app)
-
 
 
 @app.route("/")
@@ -38,26 +62,25 @@ def home():
 
     return render_template('base.html', title="World Heritage Explorer", mapbox_token=mapbox_token)
 
+
 @app.route("/historical-sites")
 def historical_search():
     query = request.args.get("query", "")
     min_year = request.args.get("minYear", "2500BC")
     max_year = request.args.get("maxYear", "2012")
 
-    
-    
     if not query:
         return jsonify([])
-    
-    
+
     results = tfidf_search(query)
     filtered_results = Filters(
-        results, 
-        min_year, 
+        results,
+        min_year,
         max_year
     ).filter_by_year()
-    
+
     return jsonify(filtered_results)
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=8080)
